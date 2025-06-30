609c191964fc7d39812267b5a597b900fb252669
RadekPotopalsky <radek.potopalsky@witte-automotive.cz>
2025-06-30 13:51:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -38,3 +38,7 @@ def get_quiz():
         quiz = json.load(f)
 
     return jsonify(quiz), 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
